79bfd35ed188b41bb9dca5818b76ed47ecb8afe1
yumozi <xueeric1223@gmail.com>
2022-09-18 21:50:19 -0400

    Complete TODO tasks

--- src/main/java/DrivableMap.java
+++ src/main/java/DrivableMap.java
@@ -27,8 +27,13 @@ class DrivableMap {
      *       in drivable_map, then add the pair to drivable_map.
      *       Return true if the Drivable was added to drivable_map.
      */
-
-
+    public boolean addDrivable(String id, Drivable drivable) {
+        if (!drivable_map.containsKey(id)) {
+            drivable_map.put(id, drivable);
+            return true;
+        }
+        return false;
+    }
 
 
     /* TODO: Write a method named hasFasterThan that takes an int (a speed)
@@ -37,7 +42,14 @@ class DrivableMap {
      * You may want to use drivable_map.keys() or drivable_map.values() to
      * iterate through drivable_map.
      */
-
+    public boolean hasFasterThan(int speed) {
+        for (Drivable drivable: drivable_map.values()) {
+            if (drivable.getMaxSpeed() >= speed) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 
 
@@ -46,8 +58,13 @@ class DrivableMap {
      *       returns a List containing all of the Tradable items in
      *       drivable_map.
      */
-
-
-
-    
+    public List<Tradable> getTradable() {
+        List<Tradable> tradables = new ArrayList<Tradable>();
+        for (Drivable drivable: drivable_map.values()) {
+            if (drivable instanceof Tradable) {
+                tradables.add((Tradable) drivable);
+            }
+        }
+        return tradables;
+    }
 }
